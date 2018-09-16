2b90d912f93351724c7ceec97900db2d65a64c68
giuem <giuemcom@gmail.com>
2018-09-16 17:27:40 +0800

    fix: cloudflare real ip conf location

--- script/cloudflare-real-ip.sh
+++ script/cloudflare-real-ip.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "#Cloudflare" > conf/conf.d/00_real_ip_cloudflare_00.conf;
+echo "#Cloudflare" > /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
 for i in `curl https://www.cloudflare.com/ips-v4`; do
-  echo "set_real_ip_from $i;" >> conf/conf.d/00_real_ip_cloudflare_00.conf;
+  echo "set_real_ip_from $i;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
 done
 for i in `curl https://www.cloudflare.com/ips-v6`; do
-  echo "set_real_ip_from $i;" >> conf/conf.d/00_real_ip_cloudflare_00.conf;
+  echo "set_real_ip_from $i;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
 done
-echo "real_ip_header CF-Connecting-IP;" >> conf/conf.d/00_real_ip_cloudflare_00.conf;
+echo "real_ip_header CF-Connecting-IP;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
